bbbfe2ed6e92ce8f6f98c72f65e71ae8f8710aaf
bootleq <bootleq@gmail.com>
2022-11-28 14:04:22 +0800

    Add jest related packages
    Going to add some unit test.
    
    Use another babel presets for test.

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,25 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          firefox: 57
-        },
-        modules: false
-      }
-    ],
-    "@babel/preset-react"
-  ]
+module.exports = api => {
+  const isTest = api.env('test');
+  let presets = [];
+
+  if (isTest) {
+    presets = [
+      ["@babel/preset-env", {targets: {node: 'current'}}]
+    ]
+  } else {
+    presets = [
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            firefox: 57
+          },
+          modules: false
+        }
+      ],
+      "@babel/preset-react"
+    ]
+  }
+
+  return { presets };
 };
