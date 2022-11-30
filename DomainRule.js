51ea5b5142908935e3272b51d713dfead1fd138f
bootleq <bootleq@gmail.com>
2022-11-30 19:53:21 +0800

    Prepare DomainRule class for domain-centric logic
    To be compatible to chrome's declarativeNetRequest API, let's change our
    design to a less flexible way.
    
    This will replace `TargetHosts` and `TargetLocales` later.

---
+++ DomainRule.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const generalLocalePattern = new RegExp([
+  /(?:[a-zA-Z]{2})/,  // en
+  /(?:[\-_]\w{4})?/,  // -US or _US (optional) or even -hant
+].map(r => r.source).join(''));
+
+class DomainRule {
+  constructor(config) {
+    this.fromLocale = config?.fromLocale ?? '*';    // zh, zh-TW, .etc. "*" for any locales
+    this.position   = config?.position   ?? 'sub.'; // "sub." | "/path"
+    this.domain     = config?.domain     ?? 'reactjs.org';
+    this.toLocale   = config?.toLocale   ?? 'en';   // leave blank for auto detection
+    this.enabled    = config?.enabled    ?? 'yes';
+  }
+}
+
+// Find "fromLocale" segement in URL, to be replaced with "toLocale".
+function match(rule, url) {
+  const { domain, fromLocale, toLocale, position, enabled } = rule;
+  const { hostname } = url;
+  let seg;
+
+  if (enabled === 'no') return;
+  if (!hostname.endsWith(domain)) return;
+
+  switch (position) {
+  case 'sub.':
+    seg = hostname.substring(0, hostname.length - domain.length);
+
+    if (seg === '') return;
+
+    if (toLocale !== '' && seg.startsWith(toLocale)) return;
+
+    if (fromLocale === '*') {
+      return seg;
+    } else {
+      return seg.startsWith(fromLocale) ? seg : undefined;
+    }
+    break;
+  case '/path':
+    seg = url.pathname.split('/')[1];
+
+    if (!seg || !generalLocalePattern.test(seg)) return;
+
+    if (fromLocale === '*') {
+      return seg.startsWith(toLocale) ? undefined : `/${seg}`;
+    } else if (seg.startsWith(fromLocale)){
+      if (toLocale === '' || !seg.startsWith(toLocale)) {
+        return `/${seg}`;
+      }
+    }
+    break;
+  }
+}
+
+function replaceMatchedSegment(rule, originURL, matched) {
+  const url = new URL(originURL);
+  const { toLocale, position } = rule;
+
+  switch (position) {
+  case 'sub.':
+    url.hostname = url.host.replace(matched, toLocale === '' ? '' : `${toLocale}.`);
+    break;
+  case '/path':
+    url.pathname = url.pathname.replace(matched, toLocale === '' ? '' : `/${toLocale}`);
+    break;
+  }
+
+  return url.href;
+}
+
+export { DomainRule, match, replaceMatchedSegment, generalLocalePattern };
+export default DomainRule;
---
+++ tests/DomainRule.test.js
@@ -0,0 +1,107 @@
+import DomainRule, { match, replaceMatchedSegment } from '../DomainRule';
+
+describe('Match', () => {
+  describe('By subdomain', () => {
+    describe('URL WITH target', () => {
+      const url = 'https://zh-hant.reactjs.org/docs/getting-started.html';
+      //                   ^^^^^^^^
+      test.each([
+        // domain       fromLocale toLocale   position enabled
+        ['reactjs.org', 'zh',      '',        'sub.',   'yes', 'zh-hant.'],
+        ['reactjs.org', 'zh-hant', '',        'sub.',   'yes', 'zh-hant.'],
+        ['reactjs.org', '*',       '',        'sub.',   'yes', 'zh-hant.'],
+        ['reactjs.org', '*',       'en',      'sub.',   'yes', 'zh-hant.'],
+        ['reactjs.org', '*',       '',        'sub.',   'yes', 'zh-hant.'], // very generic, though
+        ['reactjs.org', 'zh-hant', 'en',      'sub.',   'yes', 'zh-hant.'],
+        ['reactjs.org', 'zh',      '',        'sub.',   'no',  undefined],
+        ['reactjs.org', 'zh',      '',        '/path.', 'yes', undefined],
+        ['foo.com',     'zh',      '',        'sub.',   'yes', undefined],
+        ['reactjs.org', '*',       'zh',      'sub.',   'yes', undefined],
+        ['reactjs.org', 'zh',      'zh-hant', 'sub.',   'yes', undefined],
+      ])('Case %#: [%s, %s, %s, ...]', (domain, fromLocale, toLocale, position, enabled, expected) => {
+
+        const rule = new DomainRule({ domain, fromLocale, toLocale, position, enabled });
+        const m = match(rule, new URL(url));
+        expect(m).toBe(expected);
+      });
+    });
+
+    describe('URL WITHOUT target', () => {
+      const url = 'https://reactjs.org/docs/getting-started.html';
+
+      test.each([
+        // domain       fromLocale toLocale   position enabled
+        ['reactjs.org', 'zh',      '',        'sub.',   'yes', undefined],
+        ['reactjs.org', '*',       '',        'sub.',   'yes', undefined],
+      ])('Case %#: [%s, %s, %s, ...]', (domain, fromLocale, toLocale, position, enabled, expected) => {
+        const rule = new DomainRule({ domain, fromLocale, toLocale, position, enabled });
+        const m = match(rule, new URL(url));
+        expect(m).toBe(expected);
+      });
+    });
+
+    describe('Other subdomain', () => {
+      const url = 'https://gist.github.com/discover';
+
+      test.each([
+        // domain    fromLocale toLocale position  enabled
+        ['github.com', 'xx',      '',      'sub.',   'yes', undefined],
+        ['github.com', '*',       '',      'sub.',   'yes', 'gist.'],
+      ])('Case %#: [%s, %s, %s, ...]', (domain, fromLocale, toLocale, position, enabled, expected) => {
+        const rule = new DomainRule({ domain, fromLocale, toLocale, position, enabled });
+        const m = match(rule, new URL(url));
+        expect(m).toBe(expected);
+      });
+    });
+  });
+
+  describe('By pathname', () => {
+    describe('URL WITH target', () => {
+      const url = 'https://developer.mozilla.org/zh-TW/docs/Web/HTML';
+      //                                        ^^^^^^
+      test.each([
+        // domain     fromLocale toLocale position enabled
+        ['mozilla.org', 'zh',    '',      '/path', 'yes', '/zh-TW'],
+        ['mozilla.org', 'zh-TW', '',      '/path', 'yes', '/zh-TW'],
+        ['mozilla.org', 'zh-TW', 'en',    '/path', 'yes', '/zh-TW'],
+        ['mozilla.org', '*',     'en',    '/path', 'yes', '/zh-TW'],
+        ['mozilla.org', '*',     '',      '/path', 'yes', undefined], // avoid too generic match
+        ['mozilla.org', 'zh',    '',      '/path', 'no',  undefined],
+        ['mozilla.org', 'zh',    '',      'sub.',  'yes', undefined],
+        ['foo.com',     'zh',    '',      '/path', 'yes', undefined],
+        ['mozilla.org', 'zh',    'zh-TW', '/path', 'yes', undefined],
+      ])('Case %#: [%s, %s, %s, ...]', (domain, fromLocale, toLocale, position, enabled, expected) => {
+        const rule = new DomainRule({ domain, fromLocale, toLocale, position, enabled });
+        const m = match(rule, new URL(url));
+        expect(m).toBe(expected);
+      });
+    });
+    describe('URL WITHOUT target', () => {
+      const url = 'https://developer.mozilla.org/docs/Web/HTML';
+
+      test.each([
+        // domain     fromLocale toLocale position enabled
+        ['mozilla.org', 'zh',    '',      '/path', 'yes', undefined],
+        ['mozilla.org', '*',     '',      '/path', 'yes', undefined], // avoid too generic match
+      ])('Case %#: [%s, %s, %s, ...]', (domain, fromLocale, toLocale, position, enabled, expected) => {
+        const rule = new DomainRule({ domain, fromLocale, toLocale, position, enabled });
+        const m = match(rule, new URL(url));
+        expect(m).toBe(expected);
+      });
+    });
+  });
+});
+
+describe('replaceMatchedSegment', () => {
+  test.each([
+    //   url                                      matched    to     pos     expected
+    ['https://zh-hant.reactjs.org/docs/a?b=c#d', 'zh-hant.', '',   'sub.',  'https://reactjs.org/docs/a?b=c#d'],
+    ['https://zh-hant.reactjs.org/docs/a?b=c#d', 'zh-hant.', 'ja', 'sub.',  'https://ja.reactjs.org/docs/a?b=c#d'],
+    ['https://www.last.fm/it/about/a?b=c#d',     '/it',      '',   '/path', 'https://www.last.fm/about/a?b=c#d'],
+    ['https://www.last.fm/it/about/a?b=c#d',     '/it',      'de', '/path', 'https://www.last.fm/de/about/a?b=c#d'],
+  ])('Case %# [%s, %s, %s, ...]', (url, matched, toLocale, position, expected) => {
+    const rule = new DomainRule({ toLocale, position });
+    const result = replaceMatchedSegment(rule, url, matched);
+    expect(result).toBe(expected);
+  });
+});
