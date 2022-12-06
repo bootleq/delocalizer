b1458cbf619176f0887f65051063f24eea50b2b5
bootleq <bootleq@gmail.com>
2022-12-06 21:08:28 +0800

    Add basic eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    es6: true
+  }
+};
